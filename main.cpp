72f86c052af70d41ac68a70e8b25d5ec3af48900
pawelbaczyk <baczykpawel@gmail.com>
2015-07-07 18:46:09 +0200

    final 07.07.15

--- main.cpp
+++ main.cpp
@@ -5,16 +5,19 @@
 #include "help.h"
 #include "diag.h"
 #include "Eigen/Dense"
+#include <iomanip>
 
 using namespace std;
+
+//////////////////////////////
 
 struct PairingSP
 {
 public:
     PairingSP(int, bool);
-    int p; // from 1
-    bool spin; // 0 -- spin up, 1 -- spin down
-    double spEnergy; // single particle energy
+    int p; //from 1
+    bool spin; //0 -- spin up, 1 -- spin down
+    double spEnergy; //single particle energy
 };
 
 PairingSP::PairingSP(int _p, bool _spin) : p(_p), spin(_spin)
@@ -22,190 +25,195 @@ PairingSP::PairingSP(int _p, bool _spin) : p(_p), spin(_spin)
     spEnergy = p-1;
 }
 
+//////////////////////////////
+
 class Pairing
 {
 public:
-    vector<PairingSP> SPStates;
-    void generateSP();
+    vector<PairingSP> SP_States;
+    void generateSP(int);
     int PMax;
     double g;
 };
 
-void Pairing::generateSP()
+void Pairing::generateSP(int _PMax)//generate s.p. states (p orbits, spin up/down)
 {
-    g = 0.2;
-    PMax = 4; //TODO
+    PMax = _PMax;
     for (int p = 1; p <= PMax; p++)
     {
-        SPStates.push_back(PairingSP(p,0));
-        SPStates.push_back(PairingSP(p,1));
+        SP_States.push_back(PairingSP(p,0));//spin up
+        SP_States.push_back(PairingSP(p,1));//spin down
     }
 }
 
-class Configuration
-{
-public:
-    Configuration(int);
-    int configuration;
-};
-
-Configuration::Configuration(int config)
-{
-    configuration = config;
-}
+//////////////////////////////
 
 class System
 {
 public:
-    System(int);
-    void generateSPOrbitals();
+    System(int,int,double);
+    int A; //number of particles, have to be even for pairing
+
+    //pairing
+    Pairing spOrbitals;
+    void generateSPOrbitals(int);
+    void set_g(double);
+
+    //system
+    MatrixXd H;
+    double V2B(int,int,int,int);
+
+    //configurations
     void generateConfigurations();
     void printConfigurations();
-    Pairing spOrbitals;
-    double VMatix[8][8][8][8];//TODO
-    void generateVMatrix();
-    double deltaE();
+    vector<int> configurations;
+
+    //diagonalization
     void diagonalization();
-    void setG(double);
-    vector<double> coefficients();
-    double getCoefficient(int,int,int,int);
-private:
-    vector<Configuration> configurations;
-    int A; // number of particles, have to be even
+    void diag_printCoefficients();
+    double diag_E_GS;
+    VectorXd diag_coefficients;
+
+    //MBPT
+    void MBPT();
+    void MBPT_calculateCoefficients();
+    double MBPT_getCoefficient(int,int,int,int);
+    void MBPT_printCoefficients();
+    void MBPT_calculateDeltaE();
+    vector<double> MBPT_coefficients;
+    double MBPT_deltaE;
 };
 
-void System::setG(double _g)
+void System::generateSPOrbitals(int PMax)//generate s.p. orbitals
 {
-    spOrbitals.g = _g;
-    generateVMatrix();
+    spOrbitals.generateSP(PMax);
 }
 
-System::System(int _A) : A(_A)
+void System::set_g(double g)//set pairing g value
 {
+    spOrbitals.g = g;
 }
 
-void System::diagonalization()
+System::System(int _A, int PMax, double g) : A(_A)//generate system with A particles
 {
-    MatrixXd H;
+    generateSPOrbitals(PMax);
+    set_g(g);
+}
+
+void System::diagonalization()//perform diagonalization
+{
     generate_H(H, spOrbitals.g);
-    double E_GS;
-    diag(H, E_GS);
-    cout << E_GS << endl;
+    diag(H, diag_E_GS, diag_coefficients);
 }
 
-void System::generateSPOrbitals()
+void System::diag_printCoefficients()//print ground state eigenvector with C_0 = 1
 {
-    spOrbitals.generateSP();
-}
-
-void System::generateVMatrix()
-{
-    for (int a = 0; a < A; a++)
-    {
-        for (int b = 0; b < A; b++)
-        {
-            for (int i = A; i < spOrbitals.SPStates.size(); i++)
-            {
-                for (int j = A; j < spOrbitals.SPStates.size(); j++)
-                {
-                    if ((spOrbitals.SPStates[a].p == spOrbitals.SPStates[b].p) &&
-                            (spOrbitals.SPStates[a].spin != spOrbitals.SPStates[b].spin) &&
-                            (spOrbitals.SPStates[i].p == spOrbitals.SPStates[j].p) &&
-                            (spOrbitals.SPStates[i].spin != spOrbitals.SPStates[j].spin))
-                    {
-                        VMatix[a][b][i][j] = -0.5*spOrbitals.g;
-                        VMatix[i][j][a][b] = -0.5*spOrbitals.g;
-                    }
-                    else
-                    {
-                        VMatix[a][b][i][j] = 0.0;
-                        VMatix[i][j][a][b] = 0.0;
-                    }
-                }
-            }
-        }
-    }
-}
-
-vector<double> System::coefficients()
-{
-    vector<double> _coefficients;
-    _coefficients.push_back(getCoefficient(2,3,4,5));
-    _coefficients.push_back(getCoefficient(2,3,6,7));
-    _coefficients.push_back(getCoefficient(0,1,4,5));
-    _coefficients.push_back(getCoefficient(0,1,6,7));
-    for (int i = 0; i < _coefficients.size(); i++)
-        cout << _coefficients.at(i) << "\t";
+    for (int i = 0; i < diag_coefficients.size(); i++)
+        cout << setw(8) << diag_coefficients[i]/diag_coefficients[0] << "\t";
     cout << endl;
-    return _coefficients;
 }
 
-double System::getCoefficient(int a, int b, int i, int j)
+double System::V2B(int i,int j,int a,int b)//calculate 2body matrix element <ij|V_pairing|ab>
 {
-    double Ea = spOrbitals.SPStates.at(a).spEnergy;
-    double Eb = spOrbitals.SPStates.at(b).spEnergy;
-    double Ei = spOrbitals.SPStates.at(i).spEnergy;
-    double Ej = spOrbitals.SPStates.at(j).spEnergy;
-    return VMatix[i][j][a][b] / (Ei+Ej-Ea-Eb);
+    if ((spOrbitals.SP_States[a].p == spOrbitals.SP_States[b].p) && //same 'p'
+        (spOrbitals.SP_States[a].spin != spOrbitals.SP_States[b].spin) &&//different spin
+        (spOrbitals.SP_States[i].p == spOrbitals.SP_States[j].p) &&
+        (spOrbitals.SP_States[i].spin != spOrbitals.SP_States[j].spin))
+        return -0.5*spOrbitals.g;
+    else
+        return 0;
 }
 
-double System::deltaE()
+void System::MBPT()
 {
-    double _deltaE = 0.0;
-    for (int a = 0; a < A; a++)
+    MBPT_calculateCoefficients();
+    MBPT_calculateDeltaE();
+}
+
+void System::MBPT_calculateCoefficients()//calculate the coefficients for MBPT
+{
+    MBPT_coefficients.push_back(1.0); // C0
+    MBPT_coefficients.push_back(MBPT_getCoefficient(2,3,4,5)); // C1
+    MBPT_coefficients.push_back(MBPT_getCoefficient(2,3,6,7)); // C2
+    MBPT_coefficients.push_back(MBPT_getCoefficient(0,1,4,5)); // C3
+    MBPT_coefficients.push_back(MBPT_getCoefficient(0,1,6,7)); // C4
+    // there is no C5 -- 4p4h excitation
+}
+
+void System::MBPT_printCoefficients()//printing coefficients for MBPT
+{
+    for (int i = 0; i < MBPT_coefficients.size(); i++)
+        cout << setw(8) << MBPT_coefficients.at(i) << "\t";
+    cout << endl;
+}
+
+double System::MBPT_getCoefficient(int i, int j, int a, int b)//get coefficient C_ij^ab
+{
+    double Ea = spOrbitals.SP_States.at(a).spEnergy;
+    double Eb = spOrbitals.SP_States.at(b).spEnergy;
+    double Ei = spOrbitals.SP_States.at(i).spEnergy;
+    double Ej = spOrbitals.SP_States.at(j).spEnergy;
+    return V2B(i,j,a,b) / (Ei+Ej-Ea-Eb);
+}
+
+void System::MBPT_calculateDeltaE()//calculate correlation energy for MBPT
+{
+    MBPT_deltaE = 0.0;
+    for (int i = 0; i < A; i++)
     {
-        for (int b = 0; b < A; b++)
+        for (int j = 0; j < A; j++)
         {
-            for (int i = A; i < spOrbitals.SPStates.size(); i++)
+            for (int a = A; a < spOrbitals.SP_States.size(); a++)
             {
-                for (int j = A; j < spOrbitals.SPStates.size(); j++)
+                for (int b = A; b < spOrbitals.SP_States.size(); b++)
                 {
-                    double Ea = spOrbitals.SPStates.at(a).spEnergy;
-                    double Eb = spOrbitals.SPStates.at(b).spEnergy;
-                    double Ei = spOrbitals.SPStates.at(i).spEnergy;
-                    double Ej = spOrbitals.SPStates.at(j).spEnergy;
-                    _deltaE += VMatix[a][b][i][j] * VMatix[i][j][a][b] / (Ei+Ej-Ea-Eb);
+                    MBPT_deltaE += V2B(a,b,i,j) * MBPT_getCoefficient(i,j,a,b);
                 }
             }
         }
     }
-    return 0.25*_deltaE;
+    MBPT_deltaE *= 0.25;
 }
 
-//void System::generateConfigurations()
-//{
-//    int config = first(A);
-//    int configLast = last(spOrbitals.SPStates.size(),A);
-//    configurations.push_back(Configuration(config));
-//    do
-//    {
-//        config = next(config);
-//        //check
-//        configurations.push_back(Configuration(config));
-//    }
-//    while(config != configLast);
-//}
+void System::generateConfigurations()
+{
+    int config = first(A);
+    int configLast = last(spOrbitals.SP_States.size()/2,A);
+    configurations.push_back(config);
+    do
+    {
+        config = next(config);
+        configurations.push_back(config);
+    }
+    while(config != configLast);
+}
 
-//void System::printConfigurations()
-//{
-//    for (int i = 0; i < configurations.size(); i++)
-//        cout << bitset<8>(configurations.at(i).configuration) << endl;
-//}
+void System::printConfigurations()
+{
+    for (int i = 0; i < configurations.size(); i++)
+        cout << bitset<4>(configurations.at(i)) << endl;
+}
+
+//////////////////////////////
 
 int main()
 {
-    System system(4);
-//    system.generateConfigurations();
-//    system.printConfigurations();
-    system.generateSPOrbitals();
+    System system(4,4,0.4);//A, PMax, g
 
 //    for (double g = -2.0; g <= 2.0; g += 0.1)
 //    {
 //        system.setG(g);
-//        cout << g << "\t" << 2-system.spOrbitals.g-system.deltaE() << "\t";
+//        cout << g << "\t" << system.deltaE() << "\t";
 //        system.diagonalization();
 //    }
-    system.setG(1);
-    system.coefficients();
+
+    //system.generateConfigurations();
+    //system.printConfigurations();
+    system.MBPT();
+    system.MBPT_printCoefficients();
+    cout << system.MBPT_deltaE << endl;
+    system.diagonalization();
+    cout << system.diag_E_GS << endl;
+    system.diag_printCoefficients();
     return 0;
 }
