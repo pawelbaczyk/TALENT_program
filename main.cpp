6c246c531e45c4f43fcd18cd6a35809d74bb9ff2
pawelbaczyk <baczykpawel@gmail.com>
2015-07-07 15:21:00 +0200

    small modification

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cmath>
 #include <bitset>
+#include "help.cpp"
 
 using namespace std;
 
@@ -10,11 +11,13 @@ struct PairingSP
 public:
     PairingSP(int, bool);
     int p; // from 1
-    bool spin; //0 -- spin up, 1 -- spin down
+    bool spin; // 0 -- spin up, 1 -- spin down
+    double spEnergy; // single particle energy
 };
 
 PairingSP::PairingSP(int _p, bool _spin) : p(_p), spin(_spin)
 {
+    spEnergy = p-1;
 }
 
 class Pairing
@@ -23,7 +26,19 @@ public:
     vector<PairingSP> SPStates;
     void generateSP();
     int PMax;
+    double g;
 };
+
+void Pairing::generateSP()
+{
+    g = 0.2;
+    PMax = 4; //TODO
+    for (int p = 1; p <= PMax; p++)
+    {
+        SPStates.push_back(PairingSP(p,0));
+        SPStates.push_back(PairingSP(p,1));
+    }
+}
 
 class Configuration
 {
@@ -34,72 +49,113 @@ public:
 
 Configuration::Configuration(int config)
 {
-
     configuration = config;
 }
 
-template <class T>
 class System
 {
 public:
-    System(int, int);
+    System(int);
     void generateSPOrbitals();
     void generateConfigurations();
     void printConfigurations();
+    Pairing spOrbitals;
+    double VMatix[8][8][8][8];
+    void generateVMatrix();
+    double deltaE();
 private:
-    vector<T> SPOrbitals;
     vector<Configuration> configurations;
-    int numberSPStates;
-    int A; // have to be even
+    int A; // number of particles, have to be even
 };
 
-System::System(int _A, int _numberSPStates) : A(_A), numberSPStates(_numberSPStates)
+System::System(int _A) : A(_A)
 {
-    //SPOrbitals = vector<SPOrbital>(_numberSPStates);
 }
 
 void System::generateSPOrbitals()
 {
-    generatePairingSP(numberSPStates);
+    spOrbitals.generateSP();
 }
 
-void System::generatePairingSP(int numberSPStates)
+void System::generateVMatrix()
 {
-    for (int p = 1; p < numberSPStates/2; p++)
+    for (int a = 0; a < A; a++)
     {
-        SPOrbitals.push_back(PairingSP(p,0));
-        SPOrbitals.push_back(PairingSP(p,1));
+        for (int b = 0; b < A; b++)
+        {
+            for (int i = A; i < spOrbitals.SPStates.size(); i++)
+            {
+                for (int j = A; j < spOrbitals.SPStates.size(); j++)
+                {
+                    if ((a == b) && ((i == j)))
+                    {
+                        VMatix[a][b][i][j] = -0.5*spOrbitals.g;
+                        VMatix[i][j][a][b] = -0.5*spOrbitals.g;
+                    }
+                    else
+                    {
+                        VMatix[a][b][i][j] = 0.0;
+                        VMatix[i][j][a][b] = 0.0;
+                    }
+                }
+            }
+        }
     }
 }
 
-void System::generateConfigurations()
+double System::deltaE()
 {
-    int config = first(A);
-    int configLast = last(numberSPStates,A);
-    configurations.push_back(Configuration(config));
-    do
+    double _deltaE = 0.0;
+    for (int a = 0; a < A; a++)
     {
-        config = next(config);
-        //check
-        configurations.push_back(Configuration(config));
+        for (int b = 0; b < A; b++)
+        {
+            for (int i = A; i < spOrbitals.SPStates.size(); i++)
+            {
+                for (int j = A; j < spOrbitals.SPStates.size(); j++)
+                {
+                    double Ea = spOrbitals.SPStates.at(a).spEnergy;
+                    double Eb = spOrbitals.SPStates.at(b).spEnergy;
+                    double Ei = spOrbitals.SPStates.at(i).spEnergy;
+                    double Ej = spOrbitals.SPStates.at(j).spEnergy;
+                    _deltaE += VMatix[a][b][i][j] * VMatix[i][j][a][b] / (Ei+Ej-Ea-Eb);
+                }
+            }
+        }
     }
-    while(config != configLast);
+    return 0.25*_deltaE;
 }
 
-void System::printConfigurations()
-{
-    for (int i = 0; i < configurations.size(); i++)
-        cout << bitset<8>(configurations.at(i).configuration) << endl;
-}
+//void System::generateConfigurations()
+//{
+//    int config = first(A);
+//    int configLast = last(spOrbitals.SPStates.size(),A);
+//    configurations.push_back(Configuration(config));
+//    do
+//    {
+//        config = next(config);
+//        //check
+//        configurations.push_back(Configuration(config));
+//    }
+//    while(config != configLast);
+//}
+
+//void System::printConfigurations()
+//{
+//    for (int i = 0; i < configurations.size(); i++)
+//        cout << bitset<8>(configurations.at(i).configuration) << endl;
+//}
 
 int main()
 {
     cout << "Hello world" << endl;
     int g = 10;
-    System system(4,8);
-    system.generateConfigurations();
-    system.printConfigurations();
-
+    System system(4);
+//    system.generateConfigurations();
+//    system.printConfigurations();
+    system.generateSPOrbitals();
+    system.generateVMatrix();
+    cout << system.deltaE() << endl;
 
     return 0;
 }
