ca84063c34a17b04bf08a486aef5acfcb619acd9
wuqiang <wuqianghello@gmail.com>
2015-07-18 00:09:03 +0800

    twobody states

--- main.cpp
+++ main.cpp
@@ -6,18 +6,20 @@
 
 int main()
 {
-    Infinite infinite(28,4,0.16,2);//A,g_s,rho,nMax
-    infinite.HF_calculateE0();
-    infinite.CCD_SparseMatrices();
-    infinite.MBPT();
-    infinite.HF_cal_exact_E0();
-    cout << (infinite.HF_E0+infinite.MBPT_deltaE)/infinite.A << "\t" << (infinite.HF_E0+infinite.CCD_deltaE)/infinite.A << "\t"
-         << setprecision(10) << infinite.HF_E0/infinite.A << "\t" << infinite.HF_exact_E0 << endl;
+    Infinite infinite(14,2,0.16,2);//A,g_s,rho,nMax
+    infinite.generateTwoBody_States();
+    infinite.printTwoBody_States();
+    // infinite.HF_calculateE0();
+    // infinite.CCD_SparseMatrices();
+    // infinite.MBPT();
+    // infinite.HF_cal_exact_E0();
+    // cout << (infinite.HF_E0+infinite.MBPT_deltaE)/infinite.A << "\t" << (infinite.HF_E0+infinite.CCD_deltaE)/infinite.A << "\t"
+    //      << setprecision(10) << infinite.HF_E0/infinite.A << "\t" << infinite.HF_exact_E0 << endl;
 
-    Pairing pairing(4,8,0.5);
-    pairing.CCD_OnFlight();
-    pairing.MBPT();
-    cout << pairing.MBPT_deltaE << "\t" << pairing.CCD_deltaE << endl;
+    // Pairing pairing(4,8,0.5);
+    // pairing.CCD_OnFlight();
+    // pairing.MBPT();
+    // cout << pairing.MBPT_deltaE << "\t" << pairing.CCD_deltaE << endl;
 
     return 0;
 }
--- system.h
+++ system.h
@@ -19,69 +19,83 @@ public:
     double spEnergy; //single particle energy
 };
 
+class TwoBody_State
+{
+ public:
+ TwoBody_State(int _p,int _q):p(_p),q(_q){}
+  int p,q;
+};
+
 class System
 {
-public:
-    virtual ~System();
-    System(int);
-    int A; //number of particles, have to be even for pairing
-    int numberSP; //number of single particle orbitals
+ public:
+  virtual ~System();
+  System(int);
+  int A; //number of particles, have to be even for pairing
+  int numberSP; //number of single particle orbitals
 
-    //single particle orbitals
-    virtual void generateSP_States(int) = 0;
-    vector<SP_State*> SP_States;
-
-    //configurations
-    virtual void generateConfigurations() = 0;
-    virtual void printConfigurations() = 0;
-    vector<int> configurations;
-
-    //matrix elements
-    virtual double V1B(int,int) = 0;
-    virtual double V2B(int,int,int,int) = 0;
-
-    //diagonalization
-    MatrixXd H;
-    void generateH();
-    double getH(int,int);
-    void diagonalization();
-    void diag_printCoefficients();
-    double diag_E_GS;
-    double diag_deltaE;
-    VectorXd diag_coefficients;
-
-    //MBPT
-    void MBPT();
-    double MBPT_getCoefficient(int,int,int,int);
-    void MBPT_printCoefficients();
-    void MBPT_calculateDeltaE();
-    vector<double> MBPT_coefficients;
-    double MBPT_E_GS;
-    double MBPT_deltaE;
-
-    //CCD
-    void CCD_SparseMatrices();
-    void CCD_OnFlight();
-    double CCD_OnFlight_t(int,int,int,int);
-    MatrixXd CCD_OnFlight_t_m;
-    double CCD_deltaE;
+  //single particle orbitals
+  virtual void generateSP_States(int) = 0;
+  vector<SP_State*> SP_States;
 
 
-    //GF
-    void GF_generateMatrices();
-    void GF_diag();
-    double GF_E_GS;
-    double GF_deltaE;
-    MatrixXd GF_Sigma_static;
-    MatrixXd GF_Mat;
-    MatrixXd GF_Mstatic;
-    MatrixXd GF_Mr;
-    MatrixXd GF_Mq;
-    MatrixXd GF_Er;
-    MatrixXd GF_Eq;
-    MatrixXd GF_Z;
-    MatrixXd GF_W;
-    VectorXd GF_e;
+  //two_body states
+  virtual void generateTwoBody_States()=0;
+  vector<TwoBody_State*> TwoBody_States_hh;
+  vector<TwoBody_State*> TwoBody_States_ph;
+  vector<TwoBody_State*> TwoBody_States_pp;
+
+  //configurations
+  virtual void generateConfigurations() = 0;
+  virtual void printConfigurations() = 0;
+  vector<int> configurations;
+
+  //matrix elements
+  virtual double V1B(int,int) = 0;
+  virtual double V2B(int,int,int,int) = 0;
+
+  //diagonalization
+  MatrixXd H;
+  void generateH();
+  double getH(int,int);
+  void diagonalization();
+  void diag_printCoefficients();
+  double diag_E_GS;
+  double diag_deltaE;
+  VectorXd diag_coefficients;
+
+  //MBPT
+  void MBPT();
+  double MBPT_getCoefficient(int,int,int,int);
+  void MBPT_printCoefficients();
+  void MBPT_calculateDeltaE();
+  vector<double> MBPT_coefficients;
+  double MBPT_E_GS;
+  double MBPT_deltaE;
+
+  //CCD
+  void CCD_SparseMatrices();
+  void CCD_OnFlight();
+  double CCD_OnFlight_t(int,int,int,int);
+  MatrixXd CCD_OnFlight_t_m;
+  double CCD_deltaE;
+
+
+  //GF
+  void GF_generateMatrices();
+  void GF_diag();
+  double GF_E_GS;
+  double GF_deltaE;
+  MatrixXd GF_Sigma_static;
+  MatrixXd GF_Mat;
+  MatrixXd GF_Mstatic;
+  MatrixXd GF_Mr;
+  MatrixXd GF_Mq;
+  MatrixXd GF_Er;
+  MatrixXd GF_Eq;
+  MatrixXd GF_Z;
+  MatrixXd GF_W;
+  VectorXd GF_e;
 };
 
 #endif
