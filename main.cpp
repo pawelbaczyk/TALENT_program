2d74b0dfba3048477151ced47c3f144e193d2133
pawelbaczyk <baczykpawel@gmail.com>
2015-07-21 15:17:50 +0200

    name

--- main.cpp
+++ main.cpp
@@ -8,7 +8,42 @@ int main()
 {
   Infinite infinite(2,2,0.16,1);//A,g_s,rho,nMax
 
+  //  infinite.printSP_States();
+  //  infinite.generateTwoBody_States();
+  //   infinite.CCD_BlockMatrices();
+  //  infinite.printTwoBody_States();
+   // infinite.HF_calculateE0();//IMPORTANT;
+//    infinite.CCD_BlockMatricesIntermediates();
+//    cout << endl;
+//    infinite.CCD_BlockMatricesLadders();
+//    cout << endl;
+//    infinite.CCD_SparseMatrices();
+//    cout << endl;
+//    infinite.CCD_OnFlight();
+//    cout << endl;
+    infinite.TA_calculateE0(6);
 
+//   cout<<infinite.CCD_deltaE<<endl;
+=======
+  infinite.HF_calculateE0();//IMPORTANT;
+  infinite.CCD_BlockMatricesIntermediates();
+
+  //   infinite.CCD_BlockMatricesLadders();
+
+  // infinite.CCD_SparseMatrices();
+  //   cout<<infinite.CCD_deltaE<<endl;
+>>>>>>> aed963383f42f5d79dbef8e9a5d46e650965e75e
+  // infinite.HF_calculateE0();
+  // infinite.CCD_SparseMatrices();
+  //  infinite.MBPT();
+  //  cout<<infinite.MBPT_deltaE<<endl;
+  // cout << (infinite.HF_E0+infinite.MBPT_deltaE)/infinite.A << "\t" << (infinite.HF_E0+infinite.CCD_deltaE)/infinite.A << "\t"
+  //      << setprecision(10) << infinite.HF_E0/infinite.A << "\t" << infinite.HF_exact_E0 << endl;
+
+  // Pairing pairing(4,8,0.5);
+  // pairing.CCD_OnFlight();
+  // pairing.MBPT();
+  // cout << pairing.MBPT_deltaE << "\t" << pairing.CCD_deltaE << endl;
 
   return 0;
 }
